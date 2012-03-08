cad9634173562d5d73ab05860b903aa30defd440
Miguel Bejar <bejar37@gmail.com>
2012-03-08 17:04:42 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+SambaFm::Application.config.secret_token = 'b184f3d6a41b620ce0945de1cdb1522a78bab1eaea813d6e9459b560899eb9f552bcd680804b1c9c4a04bb96c396c327ff8d60d57f5e1e32d4e8e7cdec194fd8'
