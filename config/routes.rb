a67db968a057d54a652697bfd4ae0ec9021d6427
Miguel Bejar <bejar37@gmail.com>
2012-03-08 18:36:41 -0500

    fixing Devise setup

--- config/routes.rb
+++ config/routes.rb
@@ -50,7 +50,7 @@ SambaFm::Application.routes.draw do
 
   # You can have the root of your site routed with "root"
   # just remember to delete public/index.html.
-  # root :to => 'welcome#index'
+  root :to => 'songs#splash'
 
   # See how all your routes lay out with "rake routes"
 
