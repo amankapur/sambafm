c8570990ee07564bade750a69fae83d5c4799625
Miguel Bejar <bejar37@gmail.com>
2012-03-29 20:39:16 -0400

    initial changes

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20120310185048) do
+ActiveRecord::Schema.define(:version => 20120330002330) do
+
+  create_table "blogs", :force => true do |t|
+    t.string   "name"
+    t.string   "url"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+  end
 
   create_table "song_relationships", :force => true do |t|
     t.integer  "liker_id"
