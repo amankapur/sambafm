e007b4cd1ccffa0f9e1c65aa5524fa341a635eb9
Miguel Bejar <bejar37@gmail.com>
2012-03-29 20:42:50 -0400

    added blogs model

---
+++ app/models/blog.rb
@@ -0,0 +1,13 @@
+class Blog < ActiveRecord::Base
+end
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  url        :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
---
+++ test/unit/blog_test.rb
@@ -0,0 +1,18 @@
+require 'test_helper'
+
+class BlogTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
+# == Schema Information
+#
+# Table name: blogs
+#
+#  id         :integer         not null, primary key
+#  name       :string(255)
+#  url        :string(255)
+#  created_at :datetime        not null
+#  updated_at :datetime        not null
+#
+
