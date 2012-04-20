d73c54d1395b263207a0ed9f84a7d933fd28c464
Aman Kapur <amankapur91@gmail.com>
2012-04-20 23:14:41 +0530

    like/unlike done

--- app/assets/javascripts/song_table.js
+++ app/assets/javascripts/song_table.js
@@ -7,11 +7,4 @@ $(document).ready(function(){
       },
       supplied: "mp3"
     });
-    var CLIENT_ID = "8d4242fa1237419ca04ec11eb51007c0";
-
-    $(".play_button").click(function(){
-      var url = $(this).attr("id");
-      $("#jquery_jplayer_1").jPlayer(
-      "setMedia",{mp3: url + "?client_id=" + CLIENT_ID}).jPlayer("play");
-    });
 });
